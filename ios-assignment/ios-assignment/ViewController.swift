f19d58f72560b04eb7d04f16812569028c65f72b
diepnguyendjv <diep.nguyen@drjoy.vn>
2022-02-19 16:25:27 +0700

    init project

---
+++ ios-assignment/ios-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-assignment
+//
+//  Created by Nguyen Ngoc Diep on 2022/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
