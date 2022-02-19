62753735155a811e863c2dc02eedb74aa13b0c8c
diepnguyendjv <diep.nguyen@drjoy.vn>
2022-02-19 17:16:26 +0700

    Build Base Project structure

---
+++ ios-assignment/ios-assignment/Application/Base/ViewModel/BaseViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  BaseViewModel.swift
+//  ios-assignment
+//
+//  Created by Nguyen Ngoc Diep on 19/02/2022.
+//
+
+import Foundation
+class BaseViewModel: IBaseViewModel {
+    
+}
---
+++ ios-assignment/ios-assignment/Application/Base/ViewModel/IBaseViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  IBaseViewModel.swift
+//  ios-assignment
+//
+//  Created by Nguyen Ngoc Diep on 19/02/2022.
+//
+
+import Foundation
+protocol IBaseViewModel {
+    
+}
